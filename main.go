33fd160d9e7fd20da845c3fe8de363e6f3c613e3
Leonardo Muniz <leonardo.mdq@hotmail.com>
2024-02-05 19:32:36 +0100

    Add main.go file with package main and router initialization

---
+++ main.go
@@ -0,0 +1,8 @@
+package main // package main is the entry point of the program
+
+import "github.com/leomunizq/golang-opp/router"
+
+func main() {
+	// Call the Initialize function from the router package
+  router.Initialize()
+}
