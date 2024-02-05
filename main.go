3ac247a04d0d149e58c0b5bba2e29f701bb7ef14
Leonardo Muniz <leonardo.mdq@hotmail.com>
2024-02-05 23:46:47 +0100

    refactor(): Refactor main.go to initialize logger and handle initialization errors

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main // package main is the entry point of the program
 
-import "github.com/leomunizq/golang-opp/router"
+import (
+	"github.com/leomunizq/golang-opp/config"
+	"github.com/leomunizq/golang-opp/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+ logger = config.GetLogger("main")
+	// Call the Init function from the config package
+	err:= config.Init() 
+	if err != nil {
+
+logger.Errf("Error initializing the application: %v", err)
+return
+	}
+
 	// Call the Initialize function from the router package
   router.Initialize()
 }
