d8e75c61fc94f7586c744fc66fd0b5f33345814f
Leonardo Muniz <leonardo.mdq@hotmail.com>
2024-02-05 20:17:36 +0100

    Refactor router initialization and add route initialization function

--- router/router.go
+++ router/router.go
@@ -7,12 +7,8 @@ func Initialize(){
 
 	// Creates a gin router with default configuration
 	router := gin.Default()
-	// Define a route handler for the GET /ping
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	// Initialize the routes
+	initializeRoutes(router)
 	// Start serving the application
 	router.Run() // listen and serve on 0.0.0.0:8080
 }
