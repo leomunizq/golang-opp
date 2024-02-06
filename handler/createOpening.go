ba86a70a73d5a66a1da75b737717adc189967dfe
Leonardo Muniz <leonardo.mdq@hotmail.com>
2024-02-06 18:36:05 +0100

    feat(): Add createOpeningHandler function to handle POST requests

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -1,7 +1,10 @@
 package handler
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
+	"github.com/leomunizq/golang-opp/schemas"
 )
 
 func CreateOpeninngHandler(ctx *gin.Context) {
@@ -10,11 +13,23 @@ func CreateOpeninngHandler(ctx *gin.Context) {
 	ctx.BindJSON(&request)
 	if err := request.Validate(); err != nil {
 		logger.Errf("error validating request: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
 		return
 	}
 
-	if err := db.Create(&request).Error; err != nil {
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
 		logger.Errf("error create opening: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, err.Error())
 		return
 	}
+	sendSuccess(ctx, "create-opening-success", opening)
 }
