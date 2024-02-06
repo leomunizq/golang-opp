39a518b7452feadb50253266c3fbe7a9fee4225f
Leonardo Muniz <leonardo.mdq@hotmail.com>
2024-02-06 18:11:06 +0100

    feat(): Add CreateOpeninngRequest struct and Validate method to handler/request.go

---
+++ handler/request.go
@@ -0,0 +1,38 @@
+package handler
+
+import "fmt"
+
+func errParamIsRequired(name, typ string) error {
+	return fmt.Errorf("param: %s (type: %s) is required", name, typ)
+}
+
+type CreateOpeninngRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (r *CreateOpeninngRequest) Validate() error {
+	if r.Role == "" {
+		return errParamIsRequired("role", "string")
+	}
+	if r.Company == "" {
+		return errParamIsRequired("company", "string")
+	}
+	if r.Location == "" {
+		return errParamIsRequired("location", "string")
+	}
+	if r.Link == "" {
+		return errParamIsRequired("link", "string")
+	}
+	if r.Remote == nil {
+		return errParamIsRequired("remote", "bool")
+	}
+	if r.Salary <= 100 {
+		return errParamIsRequired("salary", "int64")
+	}
+	return nil
+}
