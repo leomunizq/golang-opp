e1b6fc31dfa5666a6615e9ba1a5c254b090342a6
Leonardo Muniz <leonardo.mdq@hotmail.com>
2024-02-06 19:47:51 +0100

    Refactor ListOpeninngsHandler to list openings from the database refactor():

--- handler/listOpening.go
+++ handler/listOpening.go
@@ -4,10 +4,15 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/leomunizq/golang-opp/schemas"
 )
 
-func ListOpeninngsHandler(ctx *gin.Context){
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "List opening",
-	})
+func ListOpeninngsHandler(ctx *gin.Context) {
+	openings := []schemas.Opening{}
+
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "Error listing openings")
+		return
+	}
+	sendSuccess(ctx, "List", openings)
 }
