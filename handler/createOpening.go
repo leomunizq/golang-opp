88f254bdda7bb2eda7bd4054e27eb63ba5cd38d3
Leonardo Muniz <leonardo.mdq@hotmail.com>
2024-02-06 18:10:44 +0100

    feat(): Add CreateOpeninngHandler function to handle POST requests for creating openings

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -1,14 +1,20 @@
 package handler
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
+func CreateOpeninngHandler(ctx *gin.Context) {
+	request := CreateOpeninngRequest{}
 
-func CreateOpeninngHandler(ctx *gin.Context){
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "POST opening",
-	})
+	ctx.BindJSON(&request)
+	if err := request.Validate(); err != nil {
+		logger.Errf("error validating request: %v", err.Error())
+		return
+	}
+
+	if err := db.Create(&request).Error; err != nil {
+		logger.Errf("error create opening: %v", err.Error())
+		return
+	}
 }
