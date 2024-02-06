87c1c9a38d47d68da4e47f7376c5ea7a8db31311
Leonardo Muniz <leonardo.mdq@hotmail.com>
2024-02-06 18:10:56 +0100

    feat(): Add imports and initialize handler variables

--- handler/handler.go
+++ handler/handler.go
@@ -1,9 +1,16 @@
 package handler
 
+import (
+	"github.com/leomunizq/golang-opp/config"
+	"gorm.io/gorm"
+)
 
+var (
+	logger *config.Logger
+	db     *gorm.DB
+)
 
-
-
-
-
-
+func InitializeHandler() {
+	logger = config.GetLogger("handler")
+	db = config.GetSQLite()
+}
