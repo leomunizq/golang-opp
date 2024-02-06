89669f9aa7b9162bb57eefd925b52bbf3cdc93b9
Leonardo Muniz <leonardo.mdq@hotmail.com>
2024-02-06 18:11:24 +0100

    refactor(): Refactor routes.go to initialize handler and group routes

--- router/routes.go
+++ router/routes.go
@@ -5,18 +5,21 @@ import (
 	"github.com/leomunizq/golang-opp/handler"
 )
 
-func initializeRoutes(router *gin.Engine){
+func initializeRoutes(router *gin.Engine) {
+	// initialize handler
+	handler.InitializeHandler()
+
 	// Create a new group of routes
-	v1 := router.Group("/api/v1");
-{
-	v1.GET("/opening", handler.GetOpeningHandler)
+	v1 := router.Group("/api/v1")
+	{
+		v1.GET("/opening", handler.GetOpeningHandler)
 
-	v1.POST("/opening", handler.CreateOpeninngHandler)
+		v1.POST("/opening", handler.CreateOpeninngHandler)
 
-	v1.DELETE("/opening", handler.DeleteOpeninngHandler)
+		v1.DELETE("/opening", handler.DeleteOpeninngHandler)
 
-	v1.PUT("/opening", handler.UpdateOpeninngHandler)
+		v1.PUT("/opening", handler.UpdateOpeninngHandler)
 
-	v1.GET("/openings", handler.ListOpeninngsHandler)
-}
+		v1.GET("/openings", handler.ListOpeninngsHandler)
+	}
 }
